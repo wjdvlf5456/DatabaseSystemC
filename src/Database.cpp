49be19512e7064d423ab478201876c0fab0f263f
최정필 <wjdvlf1407@gmail.com>
2024-04-01 13:56:19 +0900

    none

--- src/Database.cpp
+++ src/Database.cpp
@@ -7,18 +7,15 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl;
+	
+	cout << "command (list, add, get, del, exit): " << endl;
+	cout << "key: " << endl;
+	cout << "type (int, double, string, array): " << 두이; 
+	
 
 	class Database(){
 
 	}
 
-
-
-
-
-	
-	
-	
-	
 	return 0;
 }
